98af1ff5a37fc4551517d2ea87f7df74815deb5d
AJAY KUMAR DHANDE <144995723+DHANDEAJAYKUMAR@users.noreply.github.com>
2025-07-25 19:05:07 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Hero from '../components/Hero'
 import Testimonial from '../components/Testimonial'
 import Plan from '../components/Plan'
 import Footer from '../components/Footer'
-import AITools from '../components/AiTools'
+import AITools from '../components/AITools'
 
 const Home = () => {
   return (
